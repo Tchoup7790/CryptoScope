f43c8e9301c1f9461d877f59843b97f504852016
Baptiste <113362872+Tchoup7790@users.noreply.github.com>
2024-12-08 21:58:35 +0100

    style : start design implementation

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,44 @@
 <script setup lang="ts">
 import { CoinGeckoApi } from "./services/CoinGeckoApi";
 
-CoinGeckoApi.ping();
-const coins = CoinGeckoApi.getCoinsList();
+// if connexion succed, get CoinsList
+// CoinGeckoApi.ping().then(() => {
+//   CoinGeckoApi.getCoinsList();
+// });
 </script>
 
 <template>
-  <header></header>
-
-  <main></main>
+  <header>
+    <h1>Welcome to CryptoScope</h1>
+  </header>
+  <main>
+    <div></div>
+    <div></div>
+    <div></div>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+header {
+  background-color: var(--vt-c-blue);
+  display: flex;
+}
+
+main {
+  flex: 1;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 10px;
+}
+
+div {
+  height: 100%;
+  width: 100%;
+  border-radius: 12px;
+  background-color: var(--vt-c-purple);
+}
+
+main > *:last-child {
+  grid-column: 1/3;
+}
+</style>
---
+++ src/components/ChartComponents.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { data, options } from "../utils/chartConfig";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "vue-chartjs";
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+);
+</script>
+
+<template>
+  <Line id="testChart" :options="options" :data="data" />
+</template>
