b89e9d95f147d3e2011fb5d1a107f226ca35d668
Baptiste <113362872+Tchoup7790@users.noreply.github.com>
2025-01-09 23:30:37 +0100

    feat: create a service for fetch CoinGeckoAPI

---
+++ src/services/coin-gecko.service.ts
@@ -0,0 +1,51 @@
+import type { Coin } from '@/models/interfaces/coin'
+import type { CoinChart } from '@/models/interfaces/coin-chart'
+import axios from 'axios'
+
+const API_URL = 'https://api.coingecko.com/api/v3'
+
+export default class CoinGeckoService {
+  // Check if the API is reachable
+  static async ping(): Promise<boolean> {
+    try {
+      await axios.get(`${API_URL}/ping`)
+      return true
+    } catch (error) {
+      console.error(error)
+      return false
+    }
+  }
+
+  // Fetch a coin by ID
+  static async getCoinById(coinId: string): Promise<Coin | null> {
+    try {
+      const response = await axios.get(`${API_URL}/coins/${coinId}`)
+      return response.data
+    } catch (error) {
+      console.error(`Error getCoinById with id ${error} : `, error)
+      return null
+    }
+  }
+
+  // Fetch USD price of a coin by ID
+  static async getCoinPrice(coinId: string): Promise<number | null> {
+    try {
+      const response = await axios.get(`${API_URL}/simple/price/${coinId}`)
+      return response.data.usd
+    } catch (error) {
+      console.error(`Error getCoinPrice with id ${coinId} : `, error)
+      return null
+    }
+  }
+
+  // Get the history of a coin by ID and by days
+  static async getCoinHistory(coinId: string, days: number): Promise<CoinChart | null> {
+    try {
+      const response = await axios.get(`${API_URL}/coins/${coinId}/market_chart?days=${days}`)
+      return response.data
+    } catch (error) {
+      console.error(`Error getCoinHistory with id ${coinId} and days ${days} : `, error)
+      return null
+    }
+  }
+}
